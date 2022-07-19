d7ea7452909b6c66a5e451857cd9f28e9f95ae7f
hudir <hudirybw@gmail.com>
2022-07-19 12:05:33 +0200

    lunch time

--- 07.19_DB_find/server.js
+++ 07.19_DB_find/server.js
@@ -1,3 +1,11 @@
+/**
+ * Add related settings and use of application here
+ * - DB connections
+ * - middlewares
+ * - configuarations 
+ * - routes connections
+ */
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
@@ -28,15 +36,64 @@ app.post('/car/create', (req, res)=>{
     newCar.save(()=>res.send('New Car created!'))
 })
 
-
-
+// (R): Read all users from DB
 /**
- * Add related settings and use of application here
- * - DB connections
- * - middlewares
- * - configuarations 
- * - routes connections
+ * Queries: get/read/find something from database
  */
+
+// find all: Model.find(callback(err, result))
+app.get('/user/all', (req, res)=>{
+    User.find((err, doc)=>{
+        if(err) throw err;
+        res.json(doc) // arr of object
+    })
+})
+
+// find 1 user by id: findById((err, doc)=>{})
+app.get('/user/findbyid/:id', (req, res)=>{
+    User.findById(req.params.id, (err, doc)=>{
+        if(err) {
+            res.json(err)
+            // throw err
+        }
+        else res.json(doc)
+    })
+})
+
+// find 1 user by some obj {key:value}
+
+// (U): Update: modify existing data
+app.post('/user/update/:id', (req, res)=>{
+    const dataToUpdate = req.body
+    // Model.findByIdAndUpdate(id, newData, options, callback)
+    User.findByIdAndUpdate(req.params.id, dataToUpdate,{new: true}, (err, result)=>{
+        if(err) {
+            res.json(err)
+            // throw err
+        }
+        else {
+            res.json(result)
+            // User.findById(req.params.id, (err1, doc)=>{
+            //     if(err1) {
+            //         res.json(err1)
+            //         // throw err
+            //     }
+            //     else res.json(doc)
+            // })
+        }
+    })
+})
+
+// (D): delete existing data
+app.get('/user/delete/:id', (req, res)=>{
+    User.findByIdAndDelete(req.params.id, err=>{
+        res.json("The User with id " + req.params.id + " is deleted")
+    })
+})
+
+
+
+
 
 app.listen(PORT, ()=>{
     console.log('Server is running on ' + PORT)
