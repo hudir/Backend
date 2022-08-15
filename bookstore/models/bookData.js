d7ba9dfc862049039ec0ff05a8dc6b5d76bcfe40
hudir <hudirybw@gmail.com>
2022-08-15 10:34:30 +0200

    done

--- bookstore/models/bookData.js
+++ bookstore/models/bookData.js
@@ -1,7 +1,8 @@
 const authors = require('../data/authors.json')
-    , books = require('../data/books.json')
+    // , books = require('../data/books.json')
     , fixPath = require('./fixPath')
     , fs = require('fs')
+
 
 
 /**
@@ -9,26 +10,32 @@ const authors = require('../data/authors.json')
  * @like req.body
  * */ 
 const saveBookFromFormPost = book => {
-    return new Promise((resolve, reject)=>{
-        // check
-        if(books[0].books.some(x=>x.title == book.title && x.author == book.author)) reject({errorNr:5, message: 'Book already exist'}) 
-        else {
-
-            console.log(333)
-
-        const authorId = authors.filter(x=>x.name===book.author)[0].id
-        // console.log(authorId)
-        const newBook = {id: books[0].books[books[0].books.length-1].id + 1 , ...book, authorId:authorId}
-    
-        fs.writeFile(fixPath(__dirname+'../data/books.json'), JSON.stringify([{books: [...books[0].books, newBook]}]), err => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve()        
-        }      
-    })
-        }
-
+            return new Promise((resolve, reject)=>{
+                fs.readFile(fixPath(__dirname+'../data/books.json'),(err,data)=>{
+                    if(err) {
+                        res.json(err)
+                    } else {
+                const books = JSON.parse(data.toString())
+                         // check
+                if(books[0].books.some(x=>x.title == book.title && x.author == book.author)) reject({errorNr:5, message: 'Book already exist'}) 
+                else {
+        
+                    // console.log('saveBookFromFormPost')
+        
+                const authorId = authors.filter(x=>x.name===book.author)[0].id
+                // console.log(authorId)
+                const newBook = {id: books[0].books[books[0].books.length-1].id + 1 , ...book, authorId:authorId}
+            
+                fs.writeFile(fixPath(__dirname+'../data/books.json'), JSON.stringify([{books: [...books[0].books, newBook]}]), err => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()        
+                }      
+            })
+                }
+                    }       
+            })
         
     })
 }
