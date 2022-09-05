1a42cffa80be7d9e65d92ef539ff25c2ed9f10a4
hudir <hudirybw@gmail.com>
2022-09-05 11:14:24 +0200

    nice

--- 09.05_email_sender/routes/index.js
+++ 09.05_email_sender/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mail.yahoo.com",
+  port: 465,
+  auth: {
+    user : process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/email', (req, res)=>{
+  // console.log(req.body)
+  // res.json(req.body)
+  transporter.sendMail({
+    from:process.env.EMAIL_USER,
+    to: req.body.email,
+    subject: req.body.subject,
+    text: req.body.message,
+    // html: req.body.message
+  }).then(info=>res.json(info)).catch(err=>res.json(err))
+  
+})
+
+
 
 module.exports = router;
