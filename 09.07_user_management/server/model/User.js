120fe10a3c0ae5060ef111e7142a9ae83b67520a
hudir <hudirybw@gmail.com>
2022-09-07 10:17:09 +0200

    1

--- 09.07_user_management/server/model/User.js
+++ 09.07_user_management/server/model/User.js
@@ -2,6 +2,20 @@ const {model, Schema} = require('mongoose')
 
 const userSchema = new Schema({
     username: String,
+    name: {
+        firstName: {
+            type: String,
+            required: true
+        },
+        middleName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        }
+    },
     email: {
         type: String,
         required: true,
@@ -11,3 +25,5 @@ const userSchema = new Schema({
         type: String
     }
 }, {collection: "users"})
+
+const User = model('User'. userSchema)
